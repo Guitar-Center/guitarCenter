0409e6c5b1d1155299aa32566908633589147ec0
VictorBayu <victorbayu85@gmail.com>
2019-12-31 01:20:11 +0700

    register+login fix, add bottom navigation,add icon bottnav

---
+++ app/src/main/java/com/example/gitarcompare/Admin.java
@@ -0,0 +1,13 @@
+package com.example.gitarcompare;
+
+public class Admin {
+    public String username;
+    public String email;
+    public Admin(){
+
+    }
+    public Admin(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/gitarcompare/MainActivity.java
+++ app/src/main/java/com/example/gitarcompare/MainActivity.java
@@ -1,17 +1,39 @@
 package com.example.gitarcompare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gitarcompare.fragments.ChartFragment;
+import com.example.gitarcompare.fragments.HomeFragment;
+import com.example.gitarcompare.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        getSupportFragmentManager().beginTransaction()
+        .replace(R.id.fragment_container, new HomeFragment())
+        .commit();
     }
 
     public void kumpulanLagu(View view) {
@@ -22,5 +44,36 @@ public class MainActivity extends AppCompatActivity {
     public void tutorialChord(View view) {
         Intent intent =  new Intent(this, TutorialChord.class);
         startActivity(intent);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.beranda :
+                fragment = new HomeFragment();
+                break;
+            case R.id.charts :
+                fragment = new ChartFragment();
+                break;
+            case R.id.user :
+                fragment = new UserFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
