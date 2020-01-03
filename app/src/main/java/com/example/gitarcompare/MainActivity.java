584a6d796b61d5c560f4941c8da5f7c85f7968b4
handika14 <achmadhandika0@gmail.com>
2020-01-03 17:05:08 +0700

    plan b

--- app/src/main/java/com/example/gitarcompare/MainActivity.java
+++ app/src/main/java/com/example/gitarcompare/MainActivity.java
@@ -1,44 +1,17 @@
 package com.example.gitarcompare;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.example.gitarcompare.fragments.ChartFragment;
-import com.example.gitarcompare.fragments.HomeFragment;
-import com.example.gitarcompare.fragments.UserFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements
-        BottomNavigationView.OnNavigationItemSelectedListener,
-        HomeFragment.OnFragmentInteractionListener,
-        ChartFragment.OnFragmentInteractionListener,
-        UserFragment.OnFragmentInteractionListener{
-
-    private FirebaseAuth.AuthStateListener authListener;
-    private FirebaseAuth auth;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadFragment(new HomeFragment());
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        getSupportFragmentManager().beginTransaction()
-        .replace(R.id.fragment_container, new HomeFragment())
-        .commit();
     }
 
     public void kumpulanLagu(View view) {
@@ -49,41 +22,5 @@ public class MainActivity extends AppCompatActivity implements
     public void tutorialChord(View view) {
         Intent intent =  new Intent(this, TutorialChord.class);
         startActivity(intent);
-    }
-
-    private boolean loadFragment(Fragment fragment){
-        if(fragment != null){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
-                    .addToBackStack(null)
-                    .commit();
-            return true;
-        }
-        return false;
-    }
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        Fragment fragment = null;
-        switch (menuItem.getItemId()){
-            case R.id.beranda :
-                fragment = new HomeFragment();
-                break;
-            case R.id.charts :
-                fragment = new ChartFragment();
-                break;
-            case R.id.user :
-                fragment = new UserFragment();
-                break;
-        }
-        return loadFragment(fragment);
-    }
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
     }
 }
--- app/src/main/java/com/example/gitarcompare/SplashActivity.java
+++ app/src/main/java/com/example/gitarcompare/SplashActivity.java
@@ -1,10 +1,10 @@
 package com.example.gitarcompare;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 finish();
             }
         },2000);
