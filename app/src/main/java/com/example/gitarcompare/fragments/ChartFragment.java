a40fee93f0f7b132dbd154cf63fc9e060f26bbf1
handika14 <achmadhandika0@gmail.com>
2020-01-03 15:49:27 +0700

    erororororororororo

--- app/src/main/java/com/example/gitarcompare/fragments/ChartFragment.java
+++ app/src/main/java/com/example/gitarcompare/fragments/ChartFragment.java
@@ -3,19 +3,14 @@ package com.example.gitarcompare.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gitarcompare.R;
 import com.example.gitarcompare.models.chart;
@@ -27,9 +22,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.widget.Toast.LENGTH_LONG;
 
 /**
  * A simple {@link Fragment} subclass.
--- app/src/main/java/com/example/gitarcompare/models/chart.java
+++ app/src/main/java/com/example/gitarcompare/models/chart.java
@@ -1,5 +1,10 @@
 package com.example.gitarcompare.models;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class chart {
     public String judul, penyanyi, gambar;
 
@@ -7,6 +12,22 @@ public class chart {
         this.judul = judul;
         this.penyanyi = penyanyi;
         this.gambar = gambar;
+    }
+
+    @Exclude
+
+    public Map<String, Object> toMap() {
+
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("judul", judul);
+
+        result.put("penyanyi", penyanyi);
+
+        result.put("gambar", gambar);
+
+        return result;
+
     }
 
     public String getJudul() {
